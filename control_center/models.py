641fe73dd463d4c237a4fc086f084eebeda6444f
Jayme McKiney <jayme.mckiney@gmail.com>
2020-03-06 04:54:46 +0000

    fixed the models to working order

--- control_center/models.py
+++ control_center/models.py
@@ -1,19 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, JSON, ForeignKey, PrimaryKeyConstraint
+from sqlalchemy import Column, Integer, String, DateTime, JSON, ForeignKey, PrimaryKeyConstraint, Boolean, ARRAY
 
 Base = declarative_base()
-
-
-class Probes(Base):
-  __tablename__ = 'probes'
-  id = Column(Integer, primary_key = True)
-  name = Column(String(50), unique = True)
-  zone_id = Column(Integer, ForeignKey('zone.id'))
-  active = Column(Boolean)
-  description = Column(String(128))
-  url = Column(String(128))
-  data_columns = Column(Array(String(20)))
-  name_mappings = Column(Array(String(20)), nullable = True)
 
 
 class Zone(Base):
@@ -21,6 +9,24 @@ class Zone(Base):
   id = Column(Integer, primary_key = True)
   name = Column(String(50), unique = True)
   description = Column(String(128))
+
+
+class Probe(Base):
+  __tablename__ = 'probes'
+  id = Column(Integer, primary_key = True)
+  name = Column(String(50), unique = True)
+  zone_id = Column(Integer, ForeignKey('zones.id'))
+  active = Column(Boolean)
+  description = Column(String(128))
+  url = Column(String(128))
+
+
+class ProbeDataMapping(Base):
+  __tablename__ = 'probe_data_mapping'
+  id = Column(Integer, primary_key = True)
+  probe_id = Column(Integer, ForeignKey('probes.id'))
+  data_column_name = Column(String(20))
+  name_mapping = Column(String(20), nullable = True)
 
 
 class DataEntry(Base):
