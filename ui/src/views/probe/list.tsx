3a6b78cfb681e5ef7f390957397af49631c78456
jayme.mckiney <jayme.mckiney@gmail.com>
2023-09-09 17:12:14 -0700

    minor improvements to list probe to parody graph list

--- ui/src/views/probe/list.tsx
+++ ui/src/views/probe/list.tsx
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 
 import Table from 'react-bootstrap/Table';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
-import { get} from '../../helpers'
+import {withRouter, get, EditIcon, request, TrashIcon} from '../../helpers'
+
 import ProbeForm  from './form'
 
 
 function ProbeEntry(props) {
   let probe = props.probeData
+  let trashButton = null
+  if (props.listEdit == true)
+    trashButton = <td onClick={props.remove}><TrashIcon /></td>
   return(
-    <tr onClick={props.onClick}>
+    <tr>
+      <td><EditIcon onClick={props.onClick} /></td>
       <td>{probe.name}</td>
       <td>{probe.description}</td>
       <td>{probe.url}</td>
       <td>{probe.active? "Active" : "Deactivated"}</td>
+      {trashButton}
     </tr>
   )
 }
@@ -22,7 +29,7 @@ function ProbeEntry(props) {
 function EditFormWrapper(props) {
   return (
     <tr>
-    <td colSpan="4">
+    <td colSpan={props.span}>
       <ProbeForm edit={true} cancel={props.cancel} probe={props.probe}/> </td>
     </tr>
   )
@@ -33,7 +40,8 @@ class ListProbes extends Component {
     super(props)
     this.state = {
       probes: null,
-      edit: null
+      editEntry: null,
+      listEdit: false
     };
   } 
 
@@ -42,12 +50,14 @@ class ListProbes extends Component {
   }
 
   startEdit = (id, event) => {
-    this.setState({edit: id});
+    this.setState({editEntry: id});
   }
 
   cancelEdit = (event) => {
-    this.setState({edit: null});
+    this.setState({editEntry: null});
   }
+
+  toggleListEdit = () => this.setState({listEdit: !this.state.listEdit})
 
   render() {
     let content =  null
@@ -55,28 +65,37 @@ class ListProbes extends Component {
       content = []
 
       for(let index in this.state.probes) {
-        if (this.state.edit !== null && this.state.edit === index) {
-          content.push(<EditFormWrapper key={index} cancel={this.cancelEdit.bind(this)} probe={this.state.probes[index]} />)
+        if (this.state.editEntry !== null && this.state.editEntry === index) {
+          content.push(<EditFormWrapper key={index} span={this.state.listEdit? 6 : 5} cancel={this.cancelEdit.bind(this)} probe={this.state.probes[index]} />)
         } else {
-          content.push(<ProbeEntry key={index} onClick={this.startEdit.bind(this, index)} probeData={this.state.probes[index]} />)
+          content.push(<ProbeEntry key={index} listEdit={this.state.listEdit} onClick={this.startEdit.bind(this, index)} probeData={this.state.probes[index]} />)
         }
       }
     }
+    let trashHeader = null
+    if (this.state.listEdit) {
+      trashHeader = <th style={{width: 20}}><TrashIcon /></th>
+    }
 
     return (
+      <>
       <Table style={{marginTop: 20}} striped bordered hover>
         <thead>
           <tr>
+            <th style={{width: 20}}><EditIcon /></th>
             <th>Name</th>
             <th>Description</th>
             <th>URL</th>
             <th>Active</th>
+            {trashHeader}
           </tr>
         </thead>
         <tbody>
           {content}
         </tbody>
       </Table>
+      <Button onClick={this.toggleListEdit}>Edit List</Button>
+      </>
     )
   }
 }
